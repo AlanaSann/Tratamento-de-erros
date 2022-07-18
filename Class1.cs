ee53cfa5edb1aabb19622299d7193f7bb8ebd44c
Alana Santos de Oliveira <asoliveira@fapex.org.br>
2022-07-18 12:01:29 -0300

    first commit

---
+++ Class1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Tratar_erros
+{
+    //classe para validar a string com os métodos
+    public static class Validador
+    {
+        //metodo apra retornar booleano,metodo para verificar se uma string é uma data válida no formato DateTime
+        public static bool VerificarData(string data)
+        {
+            try
+            {
+                //DateTime é um tipo
+                DateTime dataConvertida = Convert.ToDateTime(data);
+                return true;
+            }
+            catch
+            {
+
+                Console.WriteLine("valor não pode ser convertido");
+                return false;
+            }
+        }
+        //Booleano para verificar se um valor string pode ser convertido em inteiro
+        public static bool verificarInteiro(string inteiro)
+        {
+            try
+            {
+                int inteiroConvertido = Int32.Parse(inteiro);
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Valor não pode ser convertido");
+                return false;
+            }
+        }
+
+            //booleano para verificar se um valor string pode ser convertido em um email válido
+            public static bool verificarEmail(string email)
+            {
+                
+                Regex regex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+                Match match = regex.Match(email);//verifica se o email é válido
+
+
+            if (match.Success == true)
+                return true;
+            else
+                return false;
+            }
+               
+            //booleano para verificar se um valor string pode se converter em decimal
+             public static bool verificarDecimal(string decim)
+        {
+            try
+            {
+                decimal convertdecimal = Convert.ToDecimal(decim);
+                return true;
+            }
+            catch 
+            {
+                Console.WriteLine("Valor não pode ser convertido");
+                return false;  
+            }
+        }
+
+        }
+    }
+
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Tratar_erros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //Verifica a data
+             Console.WriteLine("Digite uma data");
+             string data = Console.ReadLine();
+             bool verificarData = Validador.VerificarData(data); //utilisa a classe.metodo
+             Console.WriteLine(verificarData);*/
+
+            //Verifica se a string pode se converter em inteiro
+           // Console.WriteLine("Digite um valor");
+           // string inteiro = Console.ReadLine();
+           // bool verificarInteiro = Validador.verificarInteiro(inteiro);
+           // Console.WriteLine(verificarInteiro);
+
+            //Verifica se a string pode ser convertida em um email válido
+            //Console.WriteLine("Digite um email");
+           // string email = Console.ReadLine();
+           // bool verificarEmail = Validador.verificarEmail(email);
+           // Console.WriteLine(verificarEmail);
+
+            //Verifica se a string pode ser convertida em um decimal válido
+            Console.WriteLine("Digite um valor");
+            string decim = Console.ReadLine();
+            bool verificarDec = Validador.verificarDecimal(decim);
+            Console.WriteLine(verificarDec);
+        }
+    }
+}
